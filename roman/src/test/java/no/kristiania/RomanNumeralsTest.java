e45f352062cecb0512bd3ad952365e88f8ef7871
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-08-29 21:31:34 +0200

    shouldTransformOneToI (failing)

---
+++ roman/src/test/java/no/kristiania/RomanNumeralsTest.java
@@ -0,0 +1,18 @@
+package no.kristiania;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumeralsTest {
+    
+    @Test
+    void shouldTransformOneToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int number) {
+        return null;
+    }
+
+}
